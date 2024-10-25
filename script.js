a6aa69f28845ec2be116bc5515b56585d2ccb76f
flowz0 <billylflowers@gmail.com>
2024-10-25 00:26:12 -0700

    addEntry for dropdown

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(
+        `#${entryDropdown.value} .input-container`
+    );
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString =
+        `
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />
+    `;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+}
+
+addEntryButton.addEventListener('click', addEntry);
